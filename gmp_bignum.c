9f8eb8293e1971b7620e8c6520c9675f812276dd
notepc <nakasan42563@gmail.com>
2018-06-07 15:22:17 +0900

    a*b mod p

--- gmp_bignum.c
+++ gmp_bignum.c
@@ -12,13 +12,14 @@ int main(){
 
   BN_CTX *ctx;
   ctx=BN_CTX_new();
-  BIGNUM *a,*b,*c,*d,*e,*order;
+  BIGNUM *a,*b,*c,*d,*e,*p,*order;
 
   a=BN_new();
   b=BN_new();
   c=BN_new();
   d=BN_new();
   e=BN_new();
+  p=BN_new();
   order=BN_new();
 
   BN_one(order);
@@ -26,52 +27,55 @@ int main(){
 
   BN_rand_range(a,order);
   BN_rand_range(b,order);
+  BN_rand_range(p,order);
   printf("a : ");
   BN_print_fp(stdout,a);
   puts("");
   printf("b : ");
   BN_print_fp(stdout,b);
   puts("");
-  printf("order : ");
-  BN_print_fp(stdout,order);
+  printf("p : ");
+  BN_print_fp(stdout,p);
   puts("");
 
   printf("-----------------------------\n");
 
-  char *AA,*BB;
-
+  char *AA,*BB,*PP;
   AA=BN_bn2hex(a);
   BB=BN_bn2hex(b);
+  PP=BN_bn2hex(p);
 
   start=omp_get_wtime();
   for(i=0;i<1000000;i++)
-    BN_mul(c,a,b,ctx);
+    BN_mul(c,a,b,p,ctx);
   end=omp_get_wtime();
-  printf("c : ");
+  printf("a*b : ");
   BN_print_fp(stdout,c);
   puts("");
-  printf("openssl : %f seconds\n",(end-start));
+  printf("openssl a*b : %f seconds\n",(end-start));
 
   BN_free(a);
   BN_free(b);
   BN_free(c);
   BN_free(d);
   BN_free(e);
+  BN_free(p);
   BN_free(order);
   printf("-----------------------------\n");
 
-  mpz_t A,B,C,D,E;
-  mpz_inits(A,B,C,D,E,NULL);
+  mpz_t A,B,C,D,E,P;
+  mpz_inits(A,B,C,D,E,P,NULL);
 
   mpz_set_str(A,AA,16);
   mpz_set_str(B,BB,16);
+  mpz_set_str(P,PP,16);
 
   start=omp_get_wtime();
   for(i=0;i<1000000;i++)
-    mpz_mul(C,A,B);
+    mpz_mul(C,A,B,P);
   end=omp_get_wtime();
-  gmp_printf("c : %ZX\n",C);
-  printf("gmp     : %f seconds\n",(end-start));
+  gmp_printf("a*b : %ZX\n",C);
+  printf("gmp    a*b : %f seconds\n",(end-start));
 
   mpz_clears(A,B,C,D,E,NULL);
 
