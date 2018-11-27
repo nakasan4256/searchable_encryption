487b977968114ae1933d7d7735629a1b955f1c8f
Desktop <nakasan42563@gmail.com>
2018-11-28 01:42:28 +0900

    ペアリングのハッシュ値をとって利用

--- testcase_pair.c
+++ testcase_pair.c
@@ -7,6 +7,7 @@
 #include<omp.h>
 #include<gmp.h>
 #include<tepla/ec.h>
+#include<openssl/sha.h>
 
 typedef struct
 {
@@ -17,19 +18,24 @@ typedef struct
 typedef struct
 {
   EC_POINT A;
-  Element B;
+  unsigned char B[SHA256_DIGEST_LENGTH];
 } Peks[1];
 
 #define public_key_init(pub,ec) do { point_init(pub->P,ec); point_init(pub->Q,ec);} while(0)
 #define public_key_clear(pub) do { point_clear(pub->P); point_clear(pub->Q);} while(0)
-#define peks_init(peks,ec,f) do{ point_init(peks->A,ec); element_init(peks->B,f);} while(0)
-#define peks_clear(peks) do{point_clear(peks->A); element_clear(peks->B);} while(0)
+#define peks_init(peks,ec) do{ point_init(peks->A,ec);} while(0)
+#define peks_clear(peks) do{point_clear(peks->A);} while(0)
+
+char os[500];
+
+void element_fprint(FILE *outputfile,Element e){
+  element_get_str(os,e);
+  fprintf(outputfile,"%s\n",os);
+}
 
 void point_fprint(FILE *outputfile,EC_POINT P){
-  unsigned char* os;
-
-  point_to_oct(os,NULL,P);
-
+  point_get_str(os,P);
+  fprintf(outputfile,"%s\n",os);
 }
 
 void private_key_create(mpz_t private_key,mpz_t p){
@@ -67,24 +73,33 @@ void keyword_encrypt(Peks peks,char *keyword,Public_Key public_key,EC_PAIRING pa
 
   EC_POINT A;
   EC_POINT hash;
+  Element B;
   point_init(A,pair->g2);
   point_init(hash,pair->g1);
+  element_init(B,pair->g3);
 
   point_mul(A,r,public_key->Q);
   hash1(hash,keyword);
-  pairing_map(peks->B,hash,A,pair);
+  pairing_map(B,hash,A,pair);
+  element_get_str(os,B);
+  SHA256(os,strlen(os),peks->B);
 
   mpz_clear(r);
   point_clear(A);
   point_clear(hash);
+  element_clear(B);
 }
 
 int test(Peks peks,EC_POINT trapdoor,EC_PAIRING pair){
   Element check;
-  int k=-1;
   element_init(check,pair->g3);
+  unsigned char hash[SHA256_DIGEST_LENGTH];
+
   pairing_map(check,trapdoor,peks->A,pair);
-  k=element_cmp(check,peks->B);
+  element_get_str(os,check);
+  SHA256(os,strlen(os),hash);
+
+  int k=memcmp(hash,peks->B,32);
 
   element_clear(check);
   return k;
@@ -115,16 +130,9 @@ int main(void){
   EC_POINT P;
   point_init(P,pair->g2);
   point_random(P);
-/*
-  unsigned char* os;
-  size_t *size;
-  os=(char *)malloc(1000);
-  size=(size_t *)malloc(10);
-  point_to_oct(os,size,P);
-  for (size_t m = 0; m < 1000; m++ ){
-    printf("%02x", os[m] );
-  }
-*/
+
+  //point_fprint(stdout,P);
+
   Public_Key public_key;
   public_key_init(public_key,pair->g2);
 
@@ -148,6 +156,10 @@ int main(void){
   printf("             Q ");
   point_print(public_key->Q);
 
+  fprintf(outputfile,"public_key : P ");
+  point_fprint(outputfile,public_key->P);
+  fprintf(outputfile,"             Q ");
+  point_fprint(outputfile,public_key->Q);
 
   start=omp_get_wtime();
   for(i=0;i<count;i++)
@@ -160,19 +172,30 @@ int main(void){
 
   char keyword[n][32];
   Peks peks[n];
+
   printf("キーワード(検索タグ)を %d 個入力してください\n",n);
   for(i=0;i<n;i++){
     printf("keyword[%d] : ",i);
     scanf("%s",keyword[i]);
     fprintf(outputfile,"keyword[%d] : %s\n",i,keyword[i]);
 
-    peks_init(peks[i],pair->g2,pair->g3);
+    peks_init(peks[i],pair->g2);
     keyword_encrypt(peks[i],keyword[i],public_key,pair,p);
 
     printf("keyword_enc : A ");
     point_print(peks[i]->A);
     printf("              B ");
-    element_print(peks[i]->B);
+    for (size_t m = 0; m < SHA256_DIGEST_LENGTH; m++ ){
+      printf("%02x", peks[i]->B[m] );
+    }
+    printf("\n");
+    fprintf(outputfile,"keyword_enc : A ");
+    point_fprint(outputfile,peks[i]->A);
+    fprintf(outputfile,"              B ");
+    for (size_t m = 0; m < SHA256_DIGEST_LENGTH; m++ ){
+      fprintf(outputfile,"%02x", peks[i]->B[m] );
+    }
+    fprintf(outputfile,"\n");
   }
 
   start=omp_get_wtime();
@@ -197,6 +220,8 @@ int main(void){
 
   printf("trapdoor : ");
   point_print(trapdoor);
+  fprintf(outputfile,"trapdoor : ");
+  point_fprint(outputfile,trapdoor);
 
   start=omp_get_wtime();
   for(i=0;i<count;i++)
